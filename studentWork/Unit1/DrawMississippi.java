c36751dadb0fec15414f50ea3094671a440e2a81
Ethan Roday <ethanr_ephrata_teals@outlook.com>
2020-10-27 22:33:09 +0000

    Mississippi project

---
+++ studentWork/Unit1/DrawMississippi.java
@@ -0,0 +1,62 @@
+package studentWork.Unit1;
+
+public class DrawMississippi {
+    public static void main(String[] args) {
+        drawM();
+        drawI();
+        drawS();
+        drawS();
+        drawI();
+        drawS();
+        drawS();
+        drawI();
+        drawP();
+        drawP();
+        drawI();
+    }
+
+    public static void drawM() {
+        System.out.println("M     M");
+        System.out.println("MM   MM");
+        System.out.println("M M M M");
+        System.out.println("M  M  M");
+        System.out.println("M     M");
+        System.out.println("M     M");
+        System.out.println("M     M");
+        System.out.println();
+    }
+
+    public static void drawI() {
+        System.out.println("IIIII");
+        System.out.println("  I");
+        System.out.println("  I");
+        System.out.println("  I");
+        System.out.println("  I");
+        System.out.println("  I");
+        System.out.println("IIIII");
+        System.out.println();
+    }
+
+    public static void drawS() {
+        System.out.println(" SSSS");
+        System.out.println("S    S");
+        System.out.println("S");
+        System.out.println(" SSSS");
+        System.out.println("     S");
+        System.out.println("S    S");
+        System.out.println(" SSSS");
+        System.out.println();
+    }
+
+    public static void drawP() {
+        System.out.println("PPPPPP");
+        System.out.println("P     P");
+        System.out.println("P     P");
+        System.out.println("PPPPPP");
+        System.out.println("P");
+        System.out.println("P");
+        System.out.println("P");
+        System.out.println("P");
+        System.out.println();
+    }
+}
